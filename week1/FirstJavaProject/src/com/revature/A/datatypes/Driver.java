4848debf1582eab5f8430dd09d997bc1f65e0e70
sophiagavrila <msophiagavrila@gmail.com>
2021-05-17 13:05:07 -0400

    add datatypes

--- week1/FirstJavaProject/src/com/revature/A/datatypes/Driver.java
+++ week1/FirstJavaProject/src/com/revature/A/datatypes/Driver.java
@@ -1,5 +1,37 @@
 package com.revature.A.datatypes;
 
 public class Driver {
+	
+	// ctrl + space + enter = shortcut
+	public static void main(String[] args) {
+		
+		// This is a single comment
+		
+		/*
+		 * This is 
+		 * a multi-line
+		 * comment (everything grayed out is ignored by the compiler)
+		 */
+		/*
+		 * Java has 8 primitive datatypes derived from C
+		 * Java is 99% OO, except for these 8 primitive data types
+		 * 
+		 * 
+		 */
+		
+		// Remember that Java is statically and strongly typed! 
+		byte reallySmallNumber = 127; // 8 bits of reserved memory -> max value is 127, min value -127.
+		short shortNumber = 32767; // 16 bits of reserved mem
+		char letter = 'm'; // 16 bits of reserved mem
+		int standardNum = 10000000; // 32 bits of reserve mem
+		float anotherNum = 93.2f; // 32 bits -- less precise than a double
+		long bigNumber = 12345678L; // 64 bits
+		double decimal = 20.45; // 64 bits of mem
+		
+		boolean isOpen = false; // 1 bit represents true or false value 
+		
+		
+		
+	}
 
 }
