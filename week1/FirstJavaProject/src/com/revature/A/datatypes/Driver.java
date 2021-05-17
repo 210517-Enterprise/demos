5d4d4cf3a2a1989c34e37551a1dfebebe5493010
sophiagavrila <msophiagavrila@gmail.com>
2021-05-17 13:47:41 -0400

    add javac guide

--- week1/FirstJavaProject/src/com/revature/A/datatypes/Driver.java
+++ week1/FirstJavaProject/src/com/revature/A/datatypes/Driver.java
@@ -12,17 +12,21 @@ public class Driver {
 		 * a multi-line
 		 * comment (everything grayed out is ignored by the compiler)
 		 */
+
 		/*
-		 * Java has 8 primitive datatypes derived from C
-		 * Java is 99% OO, except for these 8 primitive data types
+		 * Java has 8 primitive datatypes
+		 * -- these are NOT objects --
 		 * 
-		 * 
+		 * Java is 99.98% Object Oriented Except for primitive data types derived from C
+		 *
+		 * There are also non-primitive datatypes (Strings, Arrays, Objects, etc.) 
+		 * But we'll talk about those later.
 		 */
 		
 		// Remember that Java is statically and strongly typed! 
 		byte reallySmallNumber = 127; // 8 bits of reserved memory -> max value is 127, min value -127.
 		short shortNumber = 32767; // 16 bits of reserved mem
-		char letter = 'm'; // 16 bits of reserved mem
+		char letter = 'm'; // 16 bits of memory that represent a character or an ASCII character's numeric value 
 		int standardNum = 10000000; // 32 bits of reserve mem
 		float anotherNum = 93.2f; // 32 bits -- less precise than a double
 		long bigNumber = 12345678L; // 64 bits
