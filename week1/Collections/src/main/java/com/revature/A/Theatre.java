d332039d9eea7c8b136ee42cb810499599c5c64c
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2021-05-21 07:19:14 -0400

    Update Theatre.java

--- week1/Collections/src/main/java/com/revature/A/Theatre.java
+++ week1/Collections/src/main/java/com/revature/A/Theatre.java
@@ -1,54 +1,52 @@
 package com.revature.A;
 
 import java.util.ArrayList;
-import java.util.Collection; // ALMOST root interace of the API (extedns Iterable)
+import java.util.Collection; // ALMOST root interace of the API (extends Iterable Interface)
 import java.util.List; 
 
 public class Theatre {
 
 	private final String theatreName;
 	private List<Seat> seats = new ArrayList<Seat>();
-
 	
-	//ArrayList
 	/*
+	======== ArrayList ========
 	
 	Advantages
 		
-	It is not synchronized so it is fast.
-	It works fast when we have to fetch or get the elements from the list.
-	It maintains insertion order, means any element by default will be added in the end of arraylist.
-	There can be duplicates in the arraylist.
+	+ It is not synchronized so it is fast.
+	+ It works fast when we have to fetch or get the elements from the list.
+	+ It maintains insertion order, means any element by default will be added in the end of arraylist.
+	+ There can be duplicates in the arraylist.
 
 	Disadvantages
 
 	Never used Arraylist when you have to do lot of insertion and deletion as this will force arraylist 
-	to adjust the elements present in the list instead used linkedlist.
+	to adjust the elements present in the list instead used *LinkedList.
 	When you want to add elements in the last or first and the arrayList size is 0, 
 	in this case u can use linkedlist to add specifically on top and bottom of list.
 	You can't maintain uniqueness.
 	*/
 
-
-	// LinkedList
 	/*
-	 * A linked list is a common data structure made of a chain of nodes in which
-	 * each node contains a value and a pointer to the next node in the chain.
-	 * 
-	 * The head pointer points to the first node, and the last element of the list
-	 * points to null. When the list is empty, the head pointer points to null.
-	 * 
-	 * Insertion and deletion of nodes are really easier. Unlike array here we don’t
-	 * have to shift elements after insertion or deletion of an element. In linked
-	 * list we just have to update the address present in next pointer of a node.
-	 * 
-	 * TLDR, in ArrayList accessing an element takes constant time [O(1)] and adding
-	 * an element takes O(n) time [worst case]. In LinkedList adding an element
-	 * takes O(n) time and accessing also takes O(n) time but LinkedList uses more
-	 * memory than ArrayList.
+	 *======= LinkedList =========
+	 
+	 A linked list is a common data structure made of a chain of nodes in which
+	 each node contains a value and a pointer to the next node in the chain.
+	  
+	 The head pointer points to the first node, and the last element of the list
+	 points to null. When the list is empty, the head pointer points to null.
+	  
+	 Insertion and deletion of nodes are really easier. Unlike array here we donâ€™t
+	 have to shift elements after insertion or deletion of an element. In linked
+	 list we just have to update the address present in next pointer of a node.
+	  
+	 TLDR, in ArrayList accessing an element takes constant time [O(1)] and adding
+	 an element takes O(n) time [worst case]. 
+	 In LinkedList adding an element
+	 takes O(n) time and accessing also takes O(n) time but LinkedList uses more
+	 memory than ArrayList.
 	 */
-
-	// we will create a Seat class
 
 	public Theatre(String theatreName, int numRows, int seatsPerRow) {
 		this.theatreName = theatreName;
@@ -62,8 +60,8 @@ public class Theatre {
 
 				// if we have a seat like A1, i'm formatiting it so it's seat A01
 				Seat seat = new Seat(row + String.format("%02d", seatNum)); // in the case that the value is 1,
-																			// it puts a 0 infront of it A01
-				// then add the seat created to the Theatrs instance's list
+											    // it puts a 0 infront of it A01
+				// then add the seat created to the Theatre instance's list
 				seats.add(seat);
 			}
 
@@ -75,8 +73,9 @@ public class Theatre {
 		/*
 		 * BINARY SEARCH ALGORITHM
 		 * 
-		 * We control clicked Collections.binarySearch() and
-		 * then 
+		 * We ctrl + clicked Collections.binarySearch() and
+		 * then customized the target we're looking for which is in 
+		 * this case a Seat object's seatNumber
 		 */
 		
 		int low = 0;
@@ -101,33 +100,33 @@ public class Theatre {
 		System.out.println("No seat found");
 		return false; // key not found
 
-//		// imagine that the requested Seat does not exist yet
-//		Seat requestedSeat = null;
-//		
-//		/*
-//		 * BRUTE FORCE ALGORITHM
-//		 * 
-//		 * Iterate through the ENTIRE data set(lists of seat) to find 
-//		 * exactly which seat has the matching seatNumber
-//		 */
-//		
-//		int i=0; 
-//		for(Seat seat : seats) {
-//			i++;
-//			System.out.println("Iteration #" + i);
-//			if(seat.getSeatNumber().equals(seatNumber)) {
-//				requestedSeat = seat;
-//				break; // we found a match!
-//			}
-//		
-//		}
-//		
-//		if (requestedSeat == null) {
-//			System.out.println("There is no seat " + seatNumber);
-//			return false;
-//		}
-//		
-//		return requestedSeat.reserve(); // returning true or false whether the seat was reserved or not
+//		Seat requestedSeat = null;      // imagine that the requested Seat does not exist yet
+		
+		/*
+		 * BRUTE FORCE ALGORITHM
+		 * 
+		 * Iterate through the ENTIRE data set(lists of seat) to find 
+		 * exactly which seat has the matching seatNumber
+		 */
+		
+/*		int i=0; 
+		for(Seat seat : seats) {
+			i++;
+			System.out.println("Iteration #" + i);
+			if(seat.getSeatNumber().equals(seatNumber)) {
+				requestedSeat = seat;
+				break; // we found a match!
+			}
+		
+		}
+		
+		if (requestedSeat == null) {
+			System.out.println("There is no seat " + seatNumber);
+			return false;
+		}
+		
+		return requestedSeat.reserve(); // returning true or false whether the seat was reserved or not
+*/
 
 	}
 
@@ -159,7 +158,6 @@ public class Theatre {
 	 */
 
 	// Tomorrow we will implement the COMPARABLE Interface
-
 	private class Seat {
 
 		private final String seatNumber;
@@ -184,7 +182,7 @@ public class Theatre {
 			}
 		}
 
-		// cancel()
+		// cancel() reservation method
 		public boolean cancel() {
 			if (this.reserved) {
 				this.reserved = false;
@@ -194,7 +192,5 @@ public class Theatre {
 				return false;
 			}
 		}
-
 	}
-
 }
