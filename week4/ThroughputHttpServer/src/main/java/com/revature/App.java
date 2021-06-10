76a9a02e0aa7652e519e2be4cc33ff3330b2eabd
sophiagavrila <msophiagavrila@gmail.com>
2021-06-10 11:07:09 -0400

    add notes to HttpServer demo

--- week4/ThroughputHttpServer/src/main/java/com/revature/App.java
+++ week4/ThroughputHttpServer/src/main/java/com/revature/App.java
@@ -1,5 +1,9 @@
 package com.revature;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
@@ -8,140 +12,161 @@ import java.nio.file.Paths;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-
+/*
+ * com.sun.net.httpserver is not an API (there is an access restriction, and Eclipse indicate this as error).
+ * You need to add an access rule "Accessible" for the pattern com/sun/net/httpserver/**:
+ * 
+ * Follow the second answer (with 50+ upvotes):
+ * https://stackoverflow.com/questions/13155734/eclipse-cant-recognize-com-sun-net-httpserver-httpserver-package
+ */
 public class App {
-
-	private static final String INPUT_FILE = "C:\\Users\\SophieGavrila\\Desktop\\demos\\week4\\ThroughputHttpServer\\src\\main\\resources\\war_and_peace.txt";
-	
-	private static final int NUMBER_OF_THREADS = 1;
-	
-	public static void main(String[] args) throws IOException {
-		
-		String text = new String(Files.readAllBytes(Paths.get(INPUT_FILE)));
-		
-		// we will pass this string object to a startServer method (this represent the data that our server manages
-		startServer(text);
-
-	}
-	
-	public static void startServer(String text) throws IOException {
-		
-		
-		/*
-		 * 1st param: takes the address of our localhost and a port we want to listen to requests on
-		 * 
-		 * 2nd param: this is the backlog size of a queue for HTTP Server request (we'll set this to 0)
-		 * becasue we want all requests to end up in our thread pool queue
-		 */
-		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-		
-		/*
-		 * Create a context which assigns a handler object to a specific http route.
-		 * This handler handles each incoming http request and sends a repsonse
-		 */
-		server.createContext("/search", new WordCountHandler(text)); // we will create this handler class
-		
-		/*
-		 * Create the Executor which will schedule each incoming http request to 
-		 * a pool of threads
-		 */
-		Executor executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
-		
-		
-		/*
-		 * We pass the executor the Http Server
-		 */
-		server.setExecutor(executor);
-
-		server.start();
-	}
-
 	/*
-	 * Implement the WordCountHandler class itself which implements the logic of
-	 * searching for the word and counting its recurrence throughout the text.
+	 * Step 1a. Copy War and Peace into workspace.
+	 * 
+	 * Step 1b. Save the path of book resource as a final String variable;
 	 */
-	private static class WordCountHandler implements HttpHandler {
-		
-		private String text;
-		
-		public WordCountHandler(String text) {
-			this.text = text;
-		}
+    private static final String INPUT_FILE = "C:\\Users\\SophieGavrila\\Desktop\\demos\\week4\\ThroughputHttpServer\\src\\main\\resources\\war_and_peace.txt";
+    
+    // variable passed to executor to control thread pool size
+    private static final int NUMBER_OF_THREADS = 4;
 
-		@Override
-		public void handle(HttpExchange httpExchange) throws IOException {
-			
-			// Here were are caputring the word that the client is searching for
-			String query = httpExchange.getRequestURI().getQuery(); // this will generate word=talk or word=some-value-that-the-user-is-looking-for
-			
-			String[] keyValue = query.split("="); // now we've separated our query to an array
-			String action = keyValue[0];
-			String word = keyValue[1];
-			
-			
-			// Check that the query param KEY is indeed "word"
-			if(!action.equals("word")) {
-				httpExchange.sendResponseHeaders(400, 0); // essentially we're sending back to the client that the request was not good....
-				return;
-			}
-			
-			String count = countWord(word); // create a method called countWord
-			
-			// transforms the string to a byte[] 
-			byte[] response = count.getBytes();
-			// we need to make sure that our response object is capable of sending back a valid status code and response body size
-			httpExchange.sendResponseHeaders(200, response.length);
-			
-			// We will represent our repsonse body as an Output Stream
-			OutputStream outputStream = httpExchange.getResponseBody();
-			
-			// then write the response body into Http RepsonseBody output Stream
-			outputStream.write(response);
-			
-			// close the stream. This automatically sends the response to the client.
-			outputStream.close();
-			
-		}
-		
-		private String countWord(String word) {
-			// implment some logic here to check the number of times the word shows up in the text
-			System.out.println("countWord() method triggered");
-			
-			long count = 0;
-			int index = 0;
-			
-			/*
+    public static void main(String[] args) throws IOException {
+		// Step 2. Save the entire content of file into a String obj in heap.
+    	String text = new String(Files.readAllBytes(Paths.get(INPUT_FILE)));  // This method returns a byte array of the
+																			  // text file
+    	// Step 3. pass it to startServer() method -- we have yet to create this.
+        startServer(text);
+    }
+
+    public static void startServer(String text) throws IOException {
+		/*
+		 * Step 4. Create start server method by calling HttpServer.create() method.
+		 * 
+		 * 1st param: Takes address of our localhost an port we want to listen on from
+		 * com.sun.net package in the form of SocketAddress object.
+		 * 
+		 * 2nd param is backlog size which is size of queue for HTTP server requests. We
+		 * keep it at 0 because all requests should end up in the thread pool queue
+		 * instead
+		 */
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        
+        /*
+		 * Step 5. Create a context which assigns a handler object to a specific http
+		 * route. This handler handles each incoming http request and sends a response.
+		 */
+        server.createContext("/search", new WordCountHandler(text));  // we will create this handler class soon.
+
+		/*
+		 * Step 6. Create the executor which will schedule each incoming http request to
+		 * a pool of threads. Set the deault thread pool to 1.
+		 */
+        Executor executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+        
+    	// pass executor to Http Server
+        server.setExecutor(executor);
+        
+        server.start();
+        
+		/*
+		 * =============================================================== 
+		 * Once the server's start() method is called our app will 
+		 * start listening to incoming requests on port 8000
+		 * 
+		 * Every incoming req that belongs to "search" route will be handled by
+		 * WordCountHandler and executed on 1 of the threads in the fixed thread pool.
+		 * ===============================================================
+		 */
+
+    }
+
+    /* ============================================================================
+	 * Step 7. Implement WordCountHandler class itself which implements the logic of
+	 * searching for the word and counting its recurrence throughout the text.
+	 * ============================================================================
+	 */
+    private static class WordCountHandler implements HttpHandler {
+        private String text;
+
+        public WordCountHandler(String text) {
+            this.text = text;
+        }
+
+    	/*
+		 * What is HttpExchange param?
+		 * 
+		 * This class encapsulates a HTTP request received and a response to be
+		 * generated in one exchange. 
+		 * 
+		 * It provides methods for examining the request from
+		 * the client, and for building and sending the response.
+		 */
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            String query = httpExchange.getRequestURI().getQuery();
+            
+        	/* since the split generates 2 values in an array, the first would be
+			 * the string "word" and the second would be the actual word bwe want 
+			 * to search for 
+			 */ 
+            String[] keyValue = query.split("=");
+            String action = keyValue[0];
+            String word = keyValue[1];
+            
+            // Check that the query param key is indeed correct
+            if (!action.equals("word")) {
+                httpExchange.sendResponseHeaders(400, 0);
+                return;
+            }
+
+            String count = countWord(word);
+
+            // then convert the count into a byte array we can serialize and send thru HTTP
+            byte[] response = count.getBytes();
+            httpExchange.sendResponseHeaders(200, response.length);
+
+            // We will represent out response body as an OutputStream
+            OutputStream outputStream = httpExchange.getResponseBody();
+           
+ 			// then write repsonse body into http ReponseBody Output stream
+ 			outputStream.write(response);
+ 			
+ 			// when we close the stream it automatically sends the response to the client
+ 			outputStream.close();
+        }
+
+		/*
+		 * Step 8.
+		 * Create the count word method which takes in the word it needs to look for as an arg.
+		 */
+        private String countWord(String word) {
+        	
+        	System.out.println("countWord() method triggered");
+        	
+            long count = 0;
+            int index = 0;
+    		/*
 			 * Run a loop.
-			 * In every iteration look for the next appearance of the word in the text
+			 * in every iteration look for the next appearance of the word in the text
 			 */
-			
-			while (index >= 0) {
-				
-				index = text.indexOf(word, index);
-				
-				// if the index is positive, we successfully found the word in the book
-				if (index >=0) {
-					count++;;
-					index++;
-				}
+            while (index >= 0) {
+            	
+                index = text.indexOf(word, index);
 
-			}			
-			return "The word \"" + word + "\" appeared " + count + " times";
-		}
-
-	}
-	
+                // if index is positive, we successfully found the word in the book.
+                if (index >= 0) {
+                    count++;  
+                    index++; // increment index so that we can keep searching for the 
+					 		 // word again starting from the next position in in the book.
+                }
+            }
+            
+        	/* 
+			 * if the index is negative that means there are no more occurences of
+			 * the word in the book, so we exit the loop and return count;
+			 */
+            return "The word \"" + word + "\" appeared " + count + " times";
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
 
