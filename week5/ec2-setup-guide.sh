002f00c11cc3e8072099fd9c494fde89eaf27263
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2021-06-16 16:51:24 -0400

    Create ec2-setup-guide.sh

---
+++ week5/ec2-setup-guide.sh
@@ -0,0 +1,34 @@
+# 1. When you launch your instance, download your .pem key
+
+# 2. Right click > GIt Bash into whatever directory holds your .pem key and paste the command below:
+#     ssh -i mynew_key ec2-user@ec2-34-204-200-76.compute-1.amazonaws.com
+# Press enter, type yes and you're in.
+
+# /home/ec2-user is where you are (you are ec2-user)
+
+# ====== EC2 SETUP ======
+# Set up the environment for your EC2 instance
+sudo yum install java-1.8.0-openjdk-devel -y
+sudo yum install maven -y
+sudo yum install git -y
+sudo amazon-linux-extras install tomcat8.5 -y
+
+# Make a new directory to hold your project to host
+mkdir projects
+cd projects
+
+# Clone your project into your new directory
+sudo git clone <project-repo>
+
+# Package the application into deployable WAR file
+cd <project>
+sudo mvn package
+
+# Copy the WAR file to Tomcat's webapps directory to deploy
+sudo cp target/YourProject-0.0.1-SNAPSHOT.war /var/lib/tomcat/webapps/
+
+# Start Tomcat -- by default, Tomcat will run on port 8080
+sudo service tomcat start
+
+# Congrats! Your project is not running.  You can visit it at the following address in your browser:
+# http://ec2-dns.amazonaws.com:8085/YourProject-0.0.1-SNAPSHOT
