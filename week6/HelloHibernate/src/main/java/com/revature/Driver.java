eece2550444888302bc2dd674b886d29e86e8424
sophiagavrila <msophiagavrila@gmail.com>
2021-06-22 15:12:09 -0400

    add crimeDAO

--- week6/HelloHibernate/src/main/java/com/revature/Driver.java
+++ week6/HelloHibernate/src/main/java/com/revature/Driver.java
@@ -1,5 +1,8 @@
 package com.revature;
 
+import com.revature.dao.CrimeDAO;
+import com.revature.models.Crime;
+
 public class Driver {
 
 	/*
@@ -9,8 +12,29 @@ public class Driver {
 	 * using an OBJECT-ORIENTED PARADIGM.
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Running the program.....");
+		initialValues();
+		
 
+	}
+	
+	
+	public static void initialValues() {
+		
+		// Create some Crime Objects
+		Crime c1 = new Crime("Arson", "setting something ablaze");
+		Crime c2 = new Crime("Freeze", "covering a whole city in ice");
+		Crime c3 = new Crime("Time Manipulation", "freeze time, rob banks");
+		
+		// create a crimeDao
+		CrimeDAO cdao = new CrimeDAO();
+		
+		// insert the crimes into the DB
+		cdao.insert(c1);
+		cdao.insert(c2);
+		cdao.insert(c3);
+		
+		
 	}
 
 }
